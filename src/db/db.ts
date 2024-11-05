4eac81dbec6549eab8825e3da989656228d38002
Shaammiru <msyamil.hamami@gmail.com>
2024-11-05 23:15:21 +0700

    feat: change to snake_case

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,3 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(Bun.env.DATABASE_URL);
+export const db = drizzle({
+  connection: Bun.env.DATABASE_URL,
+  casing: "snake_case",
+});
