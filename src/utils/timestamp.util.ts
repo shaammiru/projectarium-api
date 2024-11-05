d1b51d2c4972e0021e05a96d55f52f41328925ce
Shaammiru <msyamil.hamami@gmail.com>
2024-11-05 23:16:08 +0700

    feat: change to default

--- src/utils/timestamp.util.ts
+++ src/utils/timestamp.util.ts
@@ -1,6 +1,6 @@
 import { timestamp } from "drizzle-orm/pg-core";
 
 export const timestampModel = {
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp().defaultNow().notNull(),
 };
